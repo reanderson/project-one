fc479afb69844e1105343cb3230c1df44ab84179
rcp043 <Rcp043@gmail.com>
2018-10-01 13:13:44 -0700

    quote done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,23 @@
 
 
-var queryURL = "https://favqs.com/api/qotd"
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-  $("body").html("<h1>" + response.quote.body + "</h1>" + "<h2>" + response.quote.author +"</h2>");
+  $(document).ready(function () {
+    var queryURL = "https://favqs.com/api/qotd"
+    
+    function loadquote(){
   
-});
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+       $("#dailyquote").html(response.quote.body + "<br/>" + response.quote.author)
+    })
+  }
 
- 
+  $("#newQuote").on("click", function() {
+    loadquote()
+  })
+  loadquote()
+  })
+
